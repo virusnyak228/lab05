e2e1bba4702e6471b7aaebd342c579b76d8819d1
virusnyak228 <virusnyak.startsev@mail.ru>
2020-12-08 21:29:28 +0200

    laba05

--- 2/src/lab05.c
+++ 2/src/lab05.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main()
 { 
-	int i;
-	for ( i = 100; i >= 0; i--) {
+	int i;		/* The loop will work as long as i <10, and after each iteration the variable i will increase by 1 */
+	for ( i = 100; i >= 0; i--) { /* The condition is checked before each repetition, that is, the loop will stop when the i variable is equal to 10 */
 	printf ("%d\n", i );
 	}
 	return 0;
--- 5/src/lab05.c
+++ 5/src/lab05.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
-int main()
+int main(void)
 {
-	for ( int i = 0; i < 10 ; i/= 2)
-	{
-	printf ("%d\n", i);
+	//10*n! (1*2*3*4*5) - factorial 5
+	const int n = 5;
+	const int a = 1*2*3*4*5;
+	//10*n! (1*2*3*4*5)
+	long long int sum = 1;
+	for ( int i = 1; i <= n; i++){
+	sum = sum*i;
 	}
 	
+	
+	
+	 	
 	return 0;
 }
